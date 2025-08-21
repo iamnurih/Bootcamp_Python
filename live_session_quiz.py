755250a240ac2e7fb8bf4ede379661a62ed17f9e
iamnurih <emailnuri@gmail.com>
2025-08-21 18:53:03 +0900

    Task: live quiz

---
+++ live_session_quiz.py
@@ -0,0 +1,32 @@
+word = "Python"
+# 예상 결과: h
+# 예상 결과: n
+
+# 여기에 코드 작성
+print(word[3])
+print(word[5])
+
+numbers = [10, 20, 30, 40, 50]
+# 예상 결과: 30
+# 예상 결과: 10
+
+# 여기에 코드 작성
+print(numbers[2])
+print(numbers)
+
+text = "Python"
+# 예상 결과: Pyt
+# 예상 결과: thon
+
+
+# 여기에 코드 작성
+print(text[0:3])
+print(text[3:])
+
+data = [10, 20, 30, 40, 50]
+# 예상 결과: [20, 30, 40]
+# 예상 결과: [50, 40, 30, 20, 10]
+
+# 여기에 코드 작성
+print(numbers[1:4])
+print(numbers[::-1])
