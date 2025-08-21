91f8b907c1422a681bf7e0387fc61c3d669c7a05
iamnurih <emailnuri@gmail.com>
2025-08-21 18:55:18 +0900

    Fix: Live Session Task

--- live_session_quiz.py
+++ live_session_quiz.py
@@ -1,32 +1,19 @@
 word = "Python"
-# 예상 결과: h
-# 예상 결과: n
-
 # 여기에 코드 작성
-print(word[3])
-print(word[5])
+print(word[3]) # 예상 결과: h
+print(word[5])# 예상 결과: n
 
 numbers = [10, 20, 30, 40, 50]
-# 예상 결과: 30
-# 예상 결과: 10
-
 # 여기에 코드 작성
-print(numbers[2])
-print(numbers)
+print(numbers[2]) # 예상 결과: 30
+print(numbers[0]) # 예상 결과: 10
 
 text = "Python"
-# 예상 결과: Pyt
-# 예상 결과: thon
-
-
 # 여기에 코드 작성
-print(text[0:3])
-print(text[3:])
+print(text[0:3]) # 예상 결과: Pyt
+print(text[2:]) # 예상 결과: thon
 
 data = [10, 20, 30, 40, 50]
-# 예상 결과: [20, 30, 40]
-# 예상 결과: [50, 40, 30, 20, 10]
-
 # 여기에 코드 작성
-print(numbers[1:4])
-print(numbers[::-1])
+print(numbers[1:4]) # 예상 결과: [20, 30, 40]
+print(numbers[::-1])# 예상 결과: [50, 40, 30, 20, 10]
